9722b746caccd8182db6f235cb2981a7b8302450
maksudmansuri <intellecttec@gmail.com>
2021-05-01 11:19:01 +0530

    second commit 3

--- eca/settings.py
+++ eca/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 import dj_database_url 
-import decouple import config
+# import decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
